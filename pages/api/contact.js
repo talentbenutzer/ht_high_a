df97c0b0fdc2112e8cc34b720d8a4f3d8413cdd5
talentbenutzer <edmund.laabs@gmail.com>
2026-05-12 14:55:23 +0200

    Use environment variable for contact recipient

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
   try {
     const data = await resend.emails.send({
       from: 'HIGH Landingpage <onboarding@resend.dev>',
-      to: 'edmund.laabs@grabner.design',
+      to: process.env.CONTACT_TO_EMAIL || 'sales@hoellental.studio',
       subject: `Anfrage via Landingpage HIGH01 von "${name}"`,
       text: mailContent,
       replyTo: email,
